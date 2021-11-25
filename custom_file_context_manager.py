b60fd90f676e5b23380b5aaa50955df03148c9e4
Madat Bay <medet.bayramov22@gmail.com>
2021-11-26 01:36:44 +0400

    custom file context manager added

---
+++ custom_file_context_manager.py
@@ -0,0 +1,14 @@
+class WritableFile:
+    def __init__(self, file_path):
+        self.file_path = file_path
+    
+    def __enter__(self):
+        self.file_obj = open(self.file_path, mode="w")
+        return self.file_obj
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        if self.file_obj:
+            self.file_obj.close()
+
+with WritableFile("sample.txt") as file:
+    file.write("Hello, world!")
